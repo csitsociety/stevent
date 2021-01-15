e2a0d51f9b01cbd7dcaa66062409ca10343c960a
Prydeton <contact@maxwellreid.tech>
2021-01-15 15:35:48 +1100

    Add queryEventsBy.js as was not added in previous commit

--- backend/routes/queryEventsBy.js
+++ backend/routes/queryEventsBy.js
@@ -1,11 +1,23 @@
 module.exports = function(app, datastore) {
     app.post('/queryEventsBy', async (req, res) => {
-        let body = JSON.parse(req.body)
+        let filters = req.body.filters
         const query = datastore.createQuery('Event')
-        for (i = 0; i < body.value.length; i++) {
-            query = query.filter(body.value[i][0], body.value[i][1], body.value[i][2])
-        }
         events = datastore.runQuery(query)
+        for (var i in filters) {
+            if (filters[i] != null) {
+                for (j = 0; j < events.length; j++) {
+                    if (i == "title") {
+                        if (!(events[j].title.toLowerCase()).includes(filters["title"].toLowerCase())) {
+                            events.remove(j)
+                        }
+                    } else if (i == "club") {
+                        if (!(events[j].clubs.toLowerCase()).includes(filters["club"].toLowerCase())) {
+                            events.remove(j)
+                        }
+                    }
+                }
+            }
+        }
         res.json({
             events
         })
