1f79cfe3bec49fb5085f023d3ad7555837ceb675
Prydeton <contact@maxwellreid.tech>
2021-01-15 14:55:05 +1100

    Small nonfunctional commit to checkout new branch

--- frontend/src/pages/Events/Events.js
+++ frontend/src/pages/Events/Events.js
@@ -11,11 +11,19 @@ import { useAuthStore } from 'stores';
 
 const Events = () => {
 	const auth = useAuthStore();
+	const onFilter = async (club = ) => {
+		setError(null);
+		try {
+			const events = await filterEvents({
+				filters: []],
+				password: values.password,
+			});
+
 	return (
 		<div>
 		<EventFilter/>
 		<EventColumnStyle>
-
+			{for }
 			<EventListing eventSrc="res/test_event.png"/>
 			<EventListing eventSrc="res/test_event.png"/>
 			<EventListing eventSrc="res/test_event.png"/>
