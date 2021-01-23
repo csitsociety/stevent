ad8e81b94627d86b22d7b69e95ea0f9cdfe54c7b
Prydeton <contact@maxwellreid.tech>
2021-01-23 15:42:12 +1100

    Add front end files

--- frontend/src/pages/Events/Events.js
+++ frontend/src/pages/Events/Events.js
@@ -10,24 +10,31 @@ import {
 import { useAuthStore } from 'stores';
 
 import event_img from 'res/test_event.png';
+import { retrieveEventsFeed } from 'services/events';
 
 const Events = () => {
-	const auth = useAuthStore();
-	const [query, setQuery] = useState('');
+	const [filter, setFilter] = useState('');
+	const [events, setEvents] = useState();
+
+	const generateEventsFeed = async () => {
+		const events = (await retrieveEventsFeed({filter: filter})).matchingEvents
+		let eventListings = []
+		for (let i = 0; i < events.length; i++) { 
+			eventListings.push(<EventListing name={events[i].name} image={event_img} date="4th Jan, 2021" description={events[i].description} hostingClubs={events[i].hostingClubs.join(" x ")}/>)
+		}
+		setEvents(eventListings)
+	}
+
+	useEffect(generateEventsFeed, [filter])
 
 	return (
 		<>
-			<EventFilter value={query} onChange={e => setQuery(e.target.value)} />
-			{query !== '' && (
-				<h4>Search results for: {query}</h4>
+			<EventFilter value={filter} onChange={e => setFilter(e.target.value)} />
+			{filter !== '' && (
+				<h4>Search results for: {filter}</h4>
 			)}
 			<EventColumnStyle>
-				<EventListing name="Test event" image={event_img} date="4th Jan, 2021" description="This is an example event used to demonstrate what an event listing looks like." />
-				<EventListing name="Test event" image={event_img} date="4th Jan, 2021" description="This is an example event used to demonstrate what an event listing looks like." />
-				<EventListing name="Test event" image={event_img} date="4th Jan, 2021" description="This is an example event used to demonstrate what an event listing looks like." />
-				<EventListing name="Test event" image={event_img} date="4th Jan, 2021" description="This is an example event used to demonstrate what an event listing looks like." />
-				<EventListing name="Test event" image={event_img} date="4th Jan, 2021" description="This is an example event used to demonstrate what an event listing looks like." />
-				<EventListing name="Test event" image={event_img} date="4th Jan, 2021" description="This is an example event used to demonstrate what an event listing looks like." />
+				{events}
 			</EventColumnStyle>
 		</>
 	)
