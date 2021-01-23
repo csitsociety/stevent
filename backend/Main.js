5912e87cf879649474739f6b95631414a2fedb86
Prydeton <contact@maxwellreid.tech>
2021-01-23 16:55:51 +1100

    Modify cloudbuild.yaml

--- backend/Main.js
+++ backend/Main.js
@@ -33,6 +33,7 @@ app.disable('x-powered-by')
 app.use(multerMid.single('file'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.static(__dirname + '/public'));
 
 app.use(session({
     store: new DatastoreStore({
