d76a4e61b5a4d5f32ace2499f34b0fbdc403deba
retroverse <ebreakey@gmail.com>
2021-02-26 12:29:54 +1100

    Dockerise

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,13 +1,31 @@
 require('dotenv').config();
 const env = process.env.NODE_ENV || 'development';
 
+const projectIdDev = process.env.PROJECT_ID
+const bucketNameDev = process.env.BUCKET_NAME
+const firebaseServiceAccountDev = process.env.FIREBASE_SERVICE_ACCOUNT || process.env.GOOGLE_APPLICATION_CREDENTIALS
+
+// Warn about missing environment variables
+if (env == 'development') {
+    if (!projectIdDev)
+        throw new Error("Expected environment variable 'PROJECT_ID'")
+    if (!bucketNameDev)
+        throw new Error("Expected environment variable 'BUCKET_NAME'")
+    if (!firebaseServiceAccountDev)
+        throw new Error("Expected environment variable 'FIREBASE_SERVICE_ACCOUNT' or 'GOOGLE_APPLICATION_CREDENTIALS'")
+} else {
+    if (!process.env.GOOGLE_APPLICATION_CREDENTIALS)
+        throw new Error("Expected environment variabke 'GOOGLE_APPLICATION_CREDENTIALS'")
+} 
+
+
 const config = {
 	development: {
 		client: 'http://localhost:3000',
 		port: 3001,
-		projectId: process.env.PROJECT_ID,
-		bucketName: process.env.BUCKET_NAME,
-		firebaseServiceAccount: process.env.FIREBASE_SERVICE_ACCOUNT || process.env.GOOGLE_APPLICATION_CREDENTIALS,
+		projectId: projectIdDev,
+		bucketName: bucketNameDev,
+		firebaseServiceAccount: firebaseServiceAccountDev
 	},
 	production: {
 		client: 'https://stevent-302609.ts.r.appspot.com',
