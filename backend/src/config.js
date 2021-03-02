b08ee3d92e357fd73faa95b8cdad6281cc6ed0b9
retroverse <ebreakey@gmail.com>
2021-03-02 12:39:22 +1100

    Fully utilise env variables on production

--- backend/src/config.js
+++ backend/src/config.js
@@ -2,37 +2,41 @@ require('dotenv').config();
 const env = process.env.NODE_ENV || 'development';
 
 console.log('Running in environment: ', env)
-const projectIdDev = process.env.PROJECT_ID
-const bucketNameDev = process.env.BUCKET_NAME
+const projectId = process.env.PROJECT_ID
+const bucketName = process.env.BUCKET_NAME
+const clientAddress = process.env.CLIENT_ADDRESS
 const firebaseServiceAccountDev = process.env.FIREBASE_SERVICE_ACCOUNT || process.env.GOOGLE_APPLICATION_CREDENTIALS
 
 // Warn about missing environment variables
 if (env == 'development') {
-    if (!projectIdDev)
+    if (!projectId)
         throw new Error("Expected environment variable 'PROJECT_ID'")
-    if (!bucketNameDev)
+    if (!bucketName)
         throw new Error("Expected environment variable 'BUCKET_NAME'")
     if (!firebaseServiceAccountDev)
         throw new Error("Expected environment variable 'FIREBASE_SERVICE_ACCOUNT' or 'GOOGLE_APPLICATION_CREDENTIALS'")
 } else {
-    //if (!process.env.GOOGLE_APPLICATION_CREDENTIALS)
-    //    throw new Error("Expected environment variabke 'GOOGLE_APPLICATION_CREDENTIALS'")
+    if (!projectId)
+        console.warn("Did not find 'PROJECT_ID' environment variable")
+    if (!bucketName)
+        console.warn("Did not find 'BUCKET_NAME' environment variable")
+    if (!clientAddress)
+        console.warn("Did not find 'CLIENT_ADDRESS' environment variable")
 } 
-
 
 const config = {
 	development: {
-		client: 'http://localhost:3000',
+		client: clientAddress || 'http://localhost:3000',
 		port: process.env.PORT || 3001,
-		projectId: projectIdDev,
-		bucketName: bucketNameDev,
+		projectId,
+		bucketName,
 		firebaseServiceAccount: firebaseServiceAccountDev
 	},
 	production: {
-		client: 'https://stevent-302609.ts.r.appspot.com',
-		port: 8080,
-		projectId: 'stevent-backend',
-		bucketName: 'stevent-backend-image-store',
+		client: clientAddress || 'https://stevent-302609.ts.r.appspot.com',
+		port: process.env.PORT || 8080,
+		projectId: projectId || 'stevent-backend',
+		bucketName: bucketName || 'stevent-backend-image-store',
 		firebaseServiceAccount: process.env.GOOGLE_APPLICATION_CREDENTIALS,
 	},
 };
