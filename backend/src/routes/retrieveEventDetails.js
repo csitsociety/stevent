9cebe43aaaa9b2c2cbc65fe7aa2ff4a9942bdb12
Prydeton <contact@maxwellreid.tech>
2021-03-16 13:36:12 +1100

    Remove auth from retrieveEventDetails route

--- backend/src/routes/retrieveEventDetails.js
+++ backend/src/routes/retrieveEventDetails.js
@@ -1,18 +1,13 @@
 module.exports = function(app, datastore, translate) {
     app.get('/retrieveEventDetails', async (req, res) => {
-    const auth = req.currentUser;
-      if (auth) {
-        const { eventID, lang} = req.query;
-        const query = datastore.createQuery("Event").filter("__key__", "=", datastore.key(["Event", parseInt(eventID)]))
-        const event = (await datastore.runQuery(query))[0][0]
-        if (lang != "au") {
-                event.description = (await translate.translate(event.description, lang))[0]
-        }
-        res.json({
-          event
-        })
-      } else {
-        return res.status(403).send('Not authorized');
-      }
+    const { eventID, lang} = req.query;
+    const query = datastore.createQuery("Event").filter("__key__", "=", datastore.key(["Event", parseInt(eventID)]))
+    const event = (await datastore.runQuery(query))[0][0]
+    if (lang != "au") {
+      event.description = (await translate.translate(event.description, lang))[0]
+    }
+    res.json({
+      event
     })
-  }
+  })
+}
