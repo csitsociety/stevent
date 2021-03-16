bff2417c95641d7b58819c77e209187cb50edf00
Prydeton <contact@maxwellreid.tech>
2021-03-16 15:27:24 +1100

    Remove translate from front and back end

--- backend/src/routes/updateUserInfo.js
+++ backend/src/routes/updateUserInfo.js
@@ -1,16 +1,15 @@
 const expectFields = require('../middleware/expectFields.js')
 
-const updateUserInfoFields = ['userID', 'username', 'description', 'lang']
+const updateUserInfoFields = ['userID', 'username', 'description']
 
 module.exports = function(app, datastore) {
 	app.post('/updateUserInfo', expectFields(updateUserInfoFields), async (req, res) => {
-		const { userID, username, description, lang } = req.body;
+		const { userID, username, description } = req.body;
 		try {
 			const userQuery = datastore.createQuery("User").filter("__key__", "=", datastore.key(["User", userID]));
 			const user = (await datastore.runQuery(userQuery))[0][0];
 			user.username = username;
 			user.description = description;
-			user.lang = lang;
 			datastore.upsert(user);
 			res.json({
 				success: true,
