6b9a9361efb60c61b127d90f615e113c2499510c
Maxwell Reid s3787033 <s3787033@student.rmit.edu.au>
2021-06-12 14:49:28 +1000

    Add gravatar support

---
+++ frontend/src/components/ProfileIcon/ProfileIcon.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {ProfilePicture} from './ProfileIconStyle'
+import gravatar from 'gravatar'
+
+const defaultIcon = "https://storage.googleapis.com/stevent-backend-image-store-development/defaultAvatar.png"
+
+const ProfileIcon  = ({profile}) => {
+    const profileIcon = profile?.icon || gravatar.url(profile?.email, {default: defaultIcon})
+    console.log(profileIcon)
+    return <ProfilePicture src={profileIcon} alt="" />
+}
+
+export default ProfileIcon
---
+++ frontend/src/components/ProfileIcon/ProfileIconStyle.js
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+export const ProfilePicture = styled.img`
+	border-radius: 1000px;
+	height: 200px;
+	width: 200px;
+	object-fit: cover;
+	background-color: #EEE;
+	border: 0;
+`;
